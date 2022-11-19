f199a592597c67cbd349ef11d66cda38926baa8a
FloralSoda <lilysoda@pm.me>
2022-11-19 15:56:47 +0000

    Fixed a crash when loading a server where a mixin injection cast to ClientPlayerEntity, when it should have been PlayerEntity

--- src/main/java/net/hydroxa/drgbeer/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/hydroxa/drgbeer/mixin/ClientPlayerEntityMixin.java
@@ -1,25 +1,17 @@
 package net.hydroxa.drgbeer.mixin;
 
-import com.mojang.authlib.GameProfile;
-import net.hydroxa.drgbeer.DRGBeerMod;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.hydroxa.drgbeer.effect.ModEffects;
-import net.hydroxa.drgbeer.sound.ModSounds;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Environment(value= EnvType.CLIENT)
 @Mixin(ClientPlayerEntity.class)
-public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
-    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
-        super(world, profile);
-    }
-
+public abstract class ClientPlayerEntityMixin{
     @Inject(method = "isSneaking()Z",at = @At("RETURN"), cancellable = true)
     public void forceSneak(CallbackInfoReturnable<Boolean> callback) {
         ClientPlayerEntity cpe = (ClientPlayerEntity)(Object)this;
--- src/main/java/net/hydroxa/drgbeer/mixin/EntityMixin.java
+++ src/main/java/net/hydroxa/drgbeer/mixin/EntityMixin.java
@@ -3,8 +3,8 @@ package net.hydroxa.drgbeer.mixin;
 import net.hydroxa.drgbeer.effect.FartyEffect;
 import net.hydroxa.drgbeer.effect.ModEffects;
 import net.hydroxa.drgbeer.sound.ModSounds;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,25 +14,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class EntityMixin {
     @Inject(method = "setYaw(F)V",at=@At("HEAD"),cancellable = true)
     public void freezeCameraYaw(CallbackInfo callback) {
-        if ((Object)this instanceof ClientPlayerEntity cpe) {
-            if (cpe.hasStatusEffect(ModEffects.FREEZE) || cpe.hasStatusEffect(ModEffects.BLACKOUT))
+        if ((Object)this instanceof PlayerEntity pe) {
+            if (pe.hasStatusEffect(ModEffects.FREEZE) || pe.hasStatusEffect(ModEffects.BLACKOUT))
                 callback.cancel();
         }
     }
     @Inject(method = "setPitch(F)V",at=@At("HEAD"),cancellable = true)
     public void freezeCameraPitch(CallbackInfo callback) {
-        if ((Object) this instanceof ClientPlayerEntity cpe) {
-            if (cpe.hasStatusEffect(ModEffects.FREEZE) || cpe.hasStatusEffect(ModEffects.BLACKOUT))
+        if ((Object)this instanceof PlayerEntity pe) {
+            if (pe.hasStatusEffect(ModEffects.FREEZE) || pe.hasStatusEffect(ModEffects.BLACKOUT))
                 callback.cancel();
         }
     }
 
     @Inject(method = "setSneaking(Z)V", at = @At("HEAD"))
     public void fartSneak(boolean setSneak, CallbackInfo cbi) {
-        if ((Object)this instanceof ClientPlayerEntity cpe) {
-            if (setSneak && cpe.hasStatusEffect(ModEffects.FARTY)) {
-                cpe.playSound(ModSounds.FART, 1, cpe.world.random.nextFloat(0.5f, 2f));
-                cpe.addVelocity(0, FartyEffect.thrust * FartyEffect.crouchMultiplier, 0);
+        if ((Object)this instanceof PlayerEntity pe) {
+            if (setSneak && pe.hasStatusEffect(ModEffects.FARTY)) {
+                pe.playSound(ModSounds.FART, 1, pe.world.random.nextFloat(0.5f, 2f));
+                pe.addVelocity(0, FartyEffect.thrust * FartyEffect.crouchMultiplier, 0);
             }
         }
     }
